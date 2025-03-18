9dc906179a88f67252c65d9629d45af4d8a3bfcb
Samuel <samuelmartinezseoane@gmail.com>
2025-03-18 10:57:21 +0100

    Inicio Repo

---
+++ src/main/java/com/mycompany/martinez_seoane_samuel_prog07_tarea/Martinez_seoane_samuel_PROG07_Tarea.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.martinez_seoane_samuel_prog07_tarea;
+
+/**
+ *
+ * @author USER
+ */
+public class Martinez_seoane_samuel_PROG07_Tarea {
+
+    public static void main(String[] args) {
+        System.out.println("Hola banco");
+    }
+}
